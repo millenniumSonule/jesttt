65b54c20f0f6c817bf7fb570b84884ffa1b64814
millenniumSonule <anshumansonuley@gmail.com>
2024-09-22 00:42:53 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [currentNumber, setCurrentNumber] = useState(0);
+
+  const increment = () => {
+    setCurrentNumber(currentNumber + 1);
+  }
+
+  const decrement = () => {
+    setCurrentNumber(currentNumber - 1);
+  }
+
+  const reset = () => {
+    setCurrentNumber(0);
+  }
+
+  const switchSigns = () => {
+    setCurrentNumber(-currentNumber)
+  }
+
   return (
-    <div className="App">
-      
+    <div className='container'>
+      <div className='container-content'>
+        <div className='components'>
+
+          <h2>
+            Count :
+          </h2>
+          <h2>{currentNumber}</h2>
+
+          <div className='button-opr'>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Restart</button>
+            <button onClick={switchSigns}>Switch Signs</button>
+          </div>
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
