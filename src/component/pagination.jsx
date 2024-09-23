9ed6e657d6ce7b1ea462bad51ecf975d72002867
millenniumSonule <anshumansonuley@gmail.com>
2024-09-24 01:52:06 +0530

    .

--- src/component/pagination.jsx
+++ src/component/pagination.jsx
@@ -1,16 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-
+import './pagination.css'
 
 const Pagination = () => {
 
-    const [data,setData] = useState('');
+    const [data, setData] = useState([]);
 
-    const fetchData = async ()=>{
-        try{
-            const x = await axios.get('https://fakestoreapi.com/products')
-            setData(x);
-        }catch(err){
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://fakestoreapi.com/products')
+            setData(response.data);
+        } catch (err) {
             console.log(err);
         }
     }
@@ -20,8 +21,20 @@ const Pagination = () => {
     }, []);
 
     return (
-        <div className='contianer'>
-           
+        <div className='container'>
+            {
+                data.map(item => (
+                    <div key={item.id} className='col-12 col-sm-6 col-md-4 col-lg-2 mb-4'>
+                        <div className='card'>
+                            <img className='img-product'
+                                src={item.image}
+                            />
+                            <p>{item.title}</p>
+                            <p>₹{item.price}</p>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
